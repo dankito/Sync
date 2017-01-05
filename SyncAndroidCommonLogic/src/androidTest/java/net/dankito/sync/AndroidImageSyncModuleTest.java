03e40d95f9b7e820c4818ab7a99a2bfb77f6a7ac
dankito <dankito@user.noreply.github.com>
2017-01-05 20:22:04 +0100

    Implemented AndroidImageSyncModuleTest

---
+++ SyncAndroidCommonLogic/src/androidTest/java/net/dankito/sync/AndroidImageSyncModuleTest.java
@@ -0,0 +1,31 @@
+package net.dankito.sync;
+
+import android.content.Context;
+
+import org.junit.Assert;
+
+/**
+ * Created by ganymed on 05/01/17.
+ */
+
+public class AndroidImageSyncModuleTest extends AndroidSyncModuleTestBase {
+
+
+  @Override
+  protected AndroidSyncModuleBase createSyncModuleToTest(Context appContext) {
+    return new AndroidImageSyncModule(appContext);
+  }
+
+  @Override
+  protected void testEntity(Entity entityToTest) {
+    Assert.assertTrue(entityToTest instanceof ImageFileEntity);
+
+    ImageFileEntity entity = (ImageFileEntity)entityToTest;
+
+    Assert.assertNotNull(entity.getFilePath());
+    Assert.assertNotNull(entity.getName());
+    Assert.assertNotNull(entity.getMimeType());
+    Assert.assertNotNull(entity.getImageTaken());
+  }
+
+}
---
+++ SyncAndroidCommonLogic/src/androidTest/java/net/dankito/sync/AndroidSyncModuleTestBase.java
@@ -0,0 +1,68 @@
+package net.dankito.sync;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by ganymed on 05/01/17.
+ */
+
+@RunWith(AndroidJUnit4.class)
+public abstract class AndroidSyncModuleTestBase {
+
+  protected AndroidSyncModuleBase underTest;
+
+  protected Context appContext = InstrumentationRegistry.getTargetContext();
+
+
+  @Before
+  public void setUp() {
+    underTest = createSyncModuleToTest(appContext);
+  }
+
+  @NonNull
+  protected abstract AndroidSyncModuleBase createSyncModuleToTest(Context appContext);
+
+
+  @Test
+  public void readAllEntitiesAsync() {
+    final List<Entity> result = new ArrayList<>();
+    final CountDownLatch countDownLatch = new CountDownLatch(1);
+
+    underTest.readAllEntitiesAsync(new ReadEntitiesCallback() {
+      @Override
+      public void done(List<Entity> entities) {
+        result.addAll(entities);
+        countDownLatch.countDown();
+      }
+    });
+
+    try { countDownLatch.await(7, TimeUnit.SECONDS); } catch(Exception e) { }
+
+    Assert.assertNotEquals(0, result.size());
+
+    for(Entity entity : result) {
+      Assert.assertNotNull(entity.getIdOnSourceDevice());
+      Assert.assertNotNull(entity.getCreatedOn());
+      Assert.assertNotNull(entity.getModifiedOn());
+
+      testEntity(entity);
+    }
+  }
+
+  protected abstract void testEntity(Entity entityToTest);
+
+
+}
