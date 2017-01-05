bbac5a48cba7228f75607c2f76a27076aefe65dd
dankito <dankito@user.noreply.github.com>
2017-01-05 23:35:05 +0100

    Added ContactEntity

---
+++ SyncDataModel/src/main/java/net/dankito/sync/ContactEntity.java
@@ -0,0 +1,136 @@
+package net.dankito.sync;
+
+/**
+ * Created by ganymed on 05/01/17.
+ */
+
+public class ContactEntity extends Entity {
+
+  protected String displayName;
+
+  protected String nickname;
+
+  protected String givenName;
+
+  protected String middleName;
+
+  protected String familyName;
+
+  protected String phoneticGivenName;
+
+  protected String phoneticMiddleName;
+
+  protected String phoneticFamilyName;
+
+  protected String phoneNumber;
+
+  protected String emailAddress;
+
+  protected String websiteUrl;
+
+  protected String note;
+
+
+  public String getDisplayName() {
+    return displayName;
+  }
+
+  public void setDisplayName(String displayName) {
+    this.displayName = displayName;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public void setNickname(String nickname) {
+    this.nickname = nickname;
+  }
+
+  public String getGivenName() {
+    return givenName;
+  }
+
+  public void setGivenName(String givenName) {
+    this.givenName = givenName;
+  }
+
+  public String getMiddleName() {
+    return middleName;
+  }
+
+  public void setMiddleName(String middleName) {
+    this.middleName = middleName;
+  }
+
+  public String getFamilyName() {
+    return familyName;
+  }
+
+  public void setFamilyName(String familyName) {
+    this.familyName = familyName;
+  }
+
+  public String getPhoneticGivenName() {
+    return phoneticGivenName;
+  }
+
+  public void setPhoneticGivenName(String phoneticGivenName) {
+    this.phoneticGivenName = phoneticGivenName;
+  }
+
+  public String getPhoneticMiddleName() {
+    return phoneticMiddleName;
+  }
+
+  public void setPhoneticMiddleName(String phoneticMiddleName) {
+    this.phoneticMiddleName = phoneticMiddleName;
+  }
+
+  public String getPhoneticFamilyName() {
+    return phoneticFamilyName;
+  }
+
+  public void setPhoneticFamilyName(String phoneticFamilyName) {
+    this.phoneticFamilyName = phoneticFamilyName;
+  }
+
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+
+  public String getEmailAddress() {
+    return emailAddress;
+  }
+
+  public void setEmailAddress(String emailAddress) {
+    this.emailAddress = emailAddress;
+  }
+
+  public String getWebsiteUrl() {
+    return websiteUrl;
+  }
+
+  public void setWebsiteUrl(String websiteUrl) {
+    this.websiteUrl = websiteUrl;
+  }
+
+  public String getNote() {
+    return note;
+  }
+
+  public void setNote(String note) {
+    this.note = note;
+  }
+
+
+  @Override
+  public String toString() {
+    return getDisplayName();
+  }
+
+}
