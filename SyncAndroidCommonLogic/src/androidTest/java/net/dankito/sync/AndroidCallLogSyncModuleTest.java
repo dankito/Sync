d2af4e68e171c50ea12db716d9461728ed58dd96
dankito <dankito@user.noreply.github.com>
2017-01-05 21:33:11 +0100

    Implemented AndroidCallLogSyncModuleTest

---
+++ SyncAndroidCommonLogic/src/androidTest/java/net/dankito/sync/AndroidCallLogSyncModuleTest.java
@@ -0,0 +1,32 @@
+package net.dankito.sync;
+
+import android.content.Context;
+
+import org.junit.Assert;
+
+/**
+ * Created by ganymed on 05/01/17.
+ */
+
+public class AndroidCallLogSyncModuleTest extends AndroidSyncModuleTestBase {
+
+
+  @Override
+  protected AndroidSyncModuleBase createSyncModuleToTest(Context appContext) {
+    return new AndroidCallLogSyncModule(appContext);
+  }
+
+  @Override
+  protected void testEntity(Entity entityToTest) {
+    Assert.assertTrue(entityToTest instanceof CallLogEntity);
+
+    CallLogEntity entity = (CallLogEntity)entityToTest;
+
+    Assert.assertNotNull(entity.getNumber());
+    Assert.assertTrue(entity.getNormalizedNumber() != null || entity.getNumber().length() == 0); // for incoming unknown numbers getNumber() is an empty String and getNormalizedNumber() is null
+    Assert.assertNotNull(entity.getDate());
+    Assert.assertTrue(entity.getDurationInSeconds() >= 0);
+    Assert.assertNotNull(entity.getType());
+  }
+
+}
