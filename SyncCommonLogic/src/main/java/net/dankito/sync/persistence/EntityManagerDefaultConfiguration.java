8ee516af962217c24afc16f2e084250bd8f2fb74
dankito <dankito@user.noreply.github.com>
2017-01-08 01:03:30 +0100

    Added EntityManagerDefaultConfiguration

---
+++ SyncCommonLogic/src/main/java/net/dankito/sync/persistence/EntityManagerDefaultConfiguration.java
@@ -0,0 +1,10 @@
+package net.dankito.sync.persistence;
+
+
+public class EntityManagerDefaultConfiguration {
+
+  public static final String DEFAULT_DATA_FOLDER = "data";
+
+  public static final int APPLICATION_DATA_MODEL_VERSION = 1;
+
+}
