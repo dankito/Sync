75e664b95c6c6b39570a47ebc7b06a7819a02427
dankito <dankito@user.noreply.github.com>
2017-01-08 01:17:13 +0100

    Added DEFAULT_ALSO_USE_PULL_REPLICATION

--- SyncCommonLogic/src/main/java/net/dankito/sync/synchronization/SynchronizationConfig.java
+++ SyncCommonLogic/src/main/java/net/dankito/sync/synchronization/SynchronizationConfig.java
@@ -5,4 +5,6 @@ public class SynchronizationConfig {
 
   public static final int DEFAULT_SYNCHRONIZATION_PORT = 32788;
 
+  public static final boolean DEFAULT_ALSO_USE_PULL_REPLICATION = true;
+
 }
