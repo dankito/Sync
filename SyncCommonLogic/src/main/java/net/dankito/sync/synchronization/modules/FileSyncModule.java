b6bde277b71997eaeff8392fc0fd7e007e1ccd43
dankito <dankito@user.noreply.github.com>
2017-01-15 14:37:11 +0100

    Implemented saveCreatedEntity()

--- SyncCommonLogic/src/main/java/net/dankito/sync/synchronization/modules/FileSyncModule.java
+++ SyncCommonLogic/src/main/java/net/dankito/sync/synchronization/modules/FileSyncModule.java
@@ -1,14 +1,26 @@
 package net.dankito.sync.synchronization.modules;
 
 
+import net.dankito.sync.FileSyncEntity;
 import net.dankito.sync.SyncEntity;
 import net.dankito.sync.SyncEntityState;
 import net.dankito.sync.SyncJobItem;
+import net.dankito.sync.SyncModuleConfiguration;
 import net.dankito.sync.synchronization.SyncEntityChangeListener;
+import net.dankito.utils.services.IFileStorageService;
 
+import java.io.File;
 import java.util.ArrayList;
 
 public class FileSyncModule extends SyncModuleBase implements ISyncModule {
+
+  protected FileHandler fileHandler;
+
+
+  public FileSyncModule(IFileStorageService fileStorageService) {
+    this.fileHandler = new FileHandler(fileStorageService);
+  }
+
 
   @Override
   public String[] getSyncEntityTypesItCanHandle() {
@@ -22,8 +34,35 @@ public class FileSyncModule extends SyncModuleBase implements ISyncModule {
 
   @Override
   public boolean synchronizedEntityRetrieved(SyncJobItem jobItem, SyncEntityState entityState) {
+    if(entityState == SyncEntityState.CREATED) {
+      return saveCreatedEntity(jobItem);
+    }
+    else if(entityState == SyncEntityState.UPDATED) {
+
+    }
+    else if(entityState == SyncEntityState.DELETED) {
+      // TODO: what about bidirectional sync modules: entities deleted on destination won't in this way deleted from source
+      if(jobItem.getSyncModuleConfiguration().isKeepDeletedEntitiesOnDestination()) {
+        return true;
+      }
+      else {
+
+      }
+    }
+
     return false;
   }
+
+
+  protected boolean saveCreatedEntity(SyncJobItem jobItem) {
+    FileSyncEntity entity = (FileSyncEntity)jobItem.getEntity();
+    SyncModuleConfiguration syncModuleConfiguration = jobItem.getSyncModuleConfiguration();
+
+    File fileDestinationPath = fileHandler.getFileDestinationPath(syncModuleConfiguration.getSourcePath(), syncModuleConfiguration.getDestinationPath(), entity.getFilePath());
+
+    return fileHandler.writeFileToDestinationPath(jobItem, fileDestinationPath);
+  }
+
 
   @Override
   public void addSyncEntityChangeListener(SyncEntityChangeListener listener) {
