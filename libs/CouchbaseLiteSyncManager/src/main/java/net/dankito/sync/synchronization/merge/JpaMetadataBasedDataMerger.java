98ecd8db8133567ec764940a589816842ac525e9
dankito <dankito@user.noreply.github.com>
2017-01-17 22:31:33 +0100

    Added @Named

--- libs/CouchbaseLiteSyncManager/src/main/java/net/dankito/sync/synchronization/merge/JpaMetadataBasedDataMerger.java
+++ libs/CouchbaseLiteSyncManager/src/main/java/net/dankito/sync/synchronization/merge/JpaMetadataBasedDataMerger.java
@@ -10,7 +10,10 @@ import net.dankito.sync.persistence.CouchbaseLiteEntityManagerBase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.inject.Named;
 
+
+@Named
 public class JpaMetadataBasedDataMerger implements IDataMerger {
 
   private static final Logger log = LoggerFactory.getLogger(JpaMetadataBasedDataMerger.class);
