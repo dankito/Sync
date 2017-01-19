bc04e064b9c7b5afadc7b8dc4487365848a4716b
dankito <dankito@user.noreply.github.com>
2017-01-19 21:17:35 +0100

    Added SyncConfigurationChanges

---
+++ SyncCommonLogic/src/main/java/net/dankito/sync/synchronization/modules/SyncConfigurationChanges.java
@@ -0,0 +1,45 @@
+package net.dankito.sync.synchronization.modules;
+
+
+import net.dankito.sync.SyncModuleConfiguration;
+import net.dankito.sync.devices.DiscoveredDevice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class SyncConfigurationChanges {
+
+  protected DiscoveredDevice remoteDevice;
+
+  protected List<SyncModuleConfiguration> addedSyncModuleConfigurations = new ArrayList<>();
+
+  protected List<SyncModuleConfiguration> removedSyncModuleConfigurations = new ArrayList<>();
+
+
+  public SyncConfigurationChanges(DiscoveredDevice remoteDevice) {
+    this.remoteDevice = remoteDevice;
+  }
+
+
+  public DiscoveredDevice getRemoteDevice() {
+    return remoteDevice;
+  }
+
+  public List<SyncModuleConfiguration> getAddedSyncModuleConfigurations() {
+    return addedSyncModuleConfigurations;
+  }
+
+  public boolean addAddedSyncModuleConfiguration(SyncModuleConfiguration addedSyncModuleConfiguration) {
+    return addedSyncModuleConfigurations.add(addedSyncModuleConfiguration);
+  }
+
+  public List<SyncModuleConfiguration> getRemovedSyncModuleConfigurations() {
+    return removedSyncModuleConfigurations;
+  }
+
+  public boolean addRemovedSyncModuleConfiguration(SyncModuleConfiguration removedSyncModuleConfiguration) {
+    return removedSyncModuleConfigurations.add(removedSyncModuleConfiguration);
+  }
+
+}
