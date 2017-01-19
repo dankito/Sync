9eb0af7f845359571516e1960f4c351b03746a12
dankito <dankito@user.noreply.github.com>
2017-01-19 19:58:08 +0100

    Implemented starting synchronization with device

--- SyncAndroidApp/src/main/java/net/dankito/sync/activities/SynchronizationSettingsActivity.java
+++ SyncAndroidApp/src/main/java/net/dankito/sync/activities/SynchronizationSettingsActivity.java
@@ -5,15 +5,22 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 
 import net.dankito.sync.MainActivity;
 import net.dankito.sync.R;
+import net.dankito.sync.SyncModuleConfiguration;
 import net.dankito.sync.adapter.SyncModuleConfigurationsAdapter;
 import net.dankito.sync.devices.DiscoveredDevice;
 import net.dankito.sync.devices.IDevicesManager;
 import net.dankito.sync.synchronization.modules.ISyncModuleConfigurationManager;
 import net.dankito.sync.synchronization.modules.SyncConfigurationWithDevice;
+import net.dankito.sync.synchronization.modules.SyncModuleSyncModuleConfigurationPair;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -66,11 +73,16 @@ public class SynchronizationSettingsActivity extends AppCompatActivity {
 
     ListView lstvwSynModuleConfigurations = (ListView)findViewById(R.id.lstvwSynModuleConfigurations);
 
+    Button btnStartSynchronizingWithDevice = (Button)findViewById(R.id.btnStartSynchronizingWithDevice);
+
     if(remoteDevice == null) { // TODO: what to do in this case?
       // by default Activities title is then set to 'Error occurred'
     }
     else {
       setTitle(remoteDevice.getDevice().getDeviceFullDisplayName());
+
+      btnStartSynchronizingWithDevice.setEnabled(true);
+      btnStartSynchronizingWithDevice.setOnClickListener(btnStartSynchronizingWithDeviceClickListener);
 
       this.syncModuleConfigurationsForDevice = syncModuleConfigurationManager.getSyncModuleConfigurationsForDevice(remoteDevice);
       lstvwSynModuleConfigurations.setAdapter(new SyncModuleConfigurationsAdapter(this, syncModuleConfigurationManager, syncModuleConfigurationsForDevice));
@@ -89,8 +101,27 @@ public class SynchronizationSettingsActivity extends AppCompatActivity {
     return super.onOptionsItemSelected(item);
   }
 
-  protected void startSynchronizingWithDevice() {
 
+  protected void startSynchronizingWithDevice() {
+    List<SyncModuleConfiguration> syncModuleConfigurations = new ArrayList<>();
+
+    for(SyncModuleSyncModuleConfigurationPair pair : syncModuleConfigurationsForDevice.getSyncModuleConfigurations()) {
+      if(pair.isEnabled()) {
+        syncModuleConfigurations.add(pair.getSyncModuleConfiguration());
+      }
+    }
+    
+    devicesManager.startSynchronizingWithDevice(remoteDevice, syncModuleConfigurations);
   }
+
+
+  protected View.OnClickListener btnStartSynchronizingWithDeviceClickListener = new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+      if(syncModuleConfigurationsForDevice.isSyncConfigurationPersisted() == false) {
+        startSynchronizingWithDevice();
+      }
+    }
+  };
 
 }
