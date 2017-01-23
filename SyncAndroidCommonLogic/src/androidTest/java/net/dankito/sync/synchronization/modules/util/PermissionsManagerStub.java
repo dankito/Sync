f24a097eff6906f438498959f4e38e97a9d35727
dankito <dankito@user.noreply.github.com>
2017-01-23 23:55:07 +0100

    Implemented checkPermissions()

--- SyncAndroidCommonLogic/src/androidTest/java/net/dankito/sync/synchronization/modules/util/PermissionsManagerStub.java
+++ SyncAndroidCommonLogic/src/androidTest/java/net/dankito/sync/synchronization/modules/util/PermissionsManagerStub.java
@@ -3,7 +3,11 @@ package net.dankito.sync.synchronization.modules.util;
 import android.support.annotation.NonNull;
 
 import net.dankito.android.util.services.IPermissionsManager;
+import net.dankito.android.util.services.MultiplePermissionsRequestCallback;
 import net.dankito.android.util.services.PermissionRequestCallback;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class PermissionsManagerStub implements IPermissionsManager {
@@ -21,6 +25,16 @@ public class PermissionsManagerStub implements IPermissionsManager {
   @Override
   public void checkPermission(String permission, String rationaleToShowToUser, PermissionRequestCallback callback) {
     callback.permissionCheckDone(permission, true);
+  }
+
+  @Override
+  public void checkPermissions(String[] permissions, String[] rationalesToShowToUser, MultiplePermissionsRequestCallback callback) {
+    Map<String, Boolean> permissionsResult = new HashMap<>();
+    for(int i = 0; i < permissions.length; i++) {
+      permissionsResult.put(permissions[i], true);
+    }
+
+    callback.permissionsCheckDone(permissionsResult);
   }
 
   @Override
