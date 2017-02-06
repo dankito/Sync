bdcffc070dfb9620b2ba1e4643ffc645d5ae3b10
dankito <dankito@user.noreply.github.com>
2017-02-06 18:24:40 +0100

    Added CallLogTableCell

---
+++ SyncJavaFX/src/main/java/net/dankito/sync/javafx/controls/cells/call_log/CallLogTableCell.java
@@ -0,0 +1,10 @@
+package net.dankito.sync.javafx.controls.cells.call_log;
+
+
+import net.dankito.sync.CallLogSyncEntity;
+import net.dankito.sync.javafx.controls.cells.TextTableCell;
+
+
+public abstract class CallLogTableCell extends TextTableCell<CallLogSyncEntity> {
+
+}
