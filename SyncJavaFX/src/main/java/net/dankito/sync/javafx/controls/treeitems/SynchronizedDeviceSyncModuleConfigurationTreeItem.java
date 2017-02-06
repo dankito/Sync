1aadd86ae4e72f9c06b73013d9d0a3c7091b3101
dankito <dankito@user.noreply.github.com>
2017-02-07 00:00:50 +0100

    Undid deriving SynchronizedDeviceSyncModuleConfigurationTreeItem from DeviceTreeItem

--- SyncJavaFX/src/main/java/net/dankito/sync/javafx/controls/treeitems/SynchronizedDeviceSyncModuleConfigurationTreeItem.java
+++ SyncJavaFX/src/main/java/net/dankito/sync/javafx/controls/treeitems/SynchronizedDeviceSyncModuleConfigurationTreeItem.java
@@ -3,21 +3,26 @@ package net.dankito.sync.javafx.controls.treeitems;
 import net.dankito.sync.devices.DiscoveredDevice;
 import net.dankito.sync.synchronization.modules.SyncModuleSyncModuleConfigurationPair;
 
+import javafx.scene.control.TreeItem;
 
-public class SynchronizedDeviceSyncModuleConfigurationTreeItem extends DeviceTreeItem {
 
+public class SynchronizedDeviceSyncModuleConfigurationTreeItem extends TreeItem<SyncModuleSyncModuleConfigurationPair> {
+
+
+  protected DiscoveredDevice device;
 
   protected SyncModuleSyncModuleConfigurationPair SyncModuleSyncModuleConfigurationPair;
 
 
   public SynchronizedDeviceSyncModuleConfigurationTreeItem(DiscoveredDevice device, SyncModuleSyncModuleConfigurationPair pair) {
-    super(device);
+    super(pair);
+    this.device = device;
     this.SyncModuleSyncModuleConfigurationPair = pair;
   }
 
 
-  public SyncModuleSyncModuleConfigurationPair getPair() {
-    return SyncModuleSyncModuleConfigurationPair;
+  public DiscoveredDevice getDevice() {
+    return device;
   }
 
 }
