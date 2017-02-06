2fc5225b54eed386a2fd70501abeeb15834889b5
dankito <dankito@user.noreply.github.com>
2017-02-06 14:33:02 +0100

    Disabled using pull replication

--- SyncCommonLogic/src/main/java/net/dankito/sync/synchronization/SynchronizationConfig.java
+++ SyncCommonLogic/src/main/java/net/dankito/sync/synchronization/SynchronizationConfig.java
@@ -5,6 +5,6 @@ public class SynchronizationConfig {
 
   public static final int DEFAULT_SYNCHRONIZATION_PORT = 32788;
 
-  public static final boolean DEFAULT_ALSO_USE_PULL_REPLICATION = true;
+  public static final boolean DEFAULT_ALSO_USE_PULL_REPLICATION = false;
 
 }
