8ec429ff44384b9d73b8d6f7a0d41e7376d0c44c
dankito <dankito@user.noreply.github.com>
2017-02-08 16:46:59 +0100

    Added Response

---
+++ SyncCommonLogic/src/main/java/net/dankito/sync/communication/message/ResponseErrorType.java
@@ -0,0 +1,11 @@
+package net.dankito.sync.communication.message;
+
+
+public enum ResponseErrorType {
+
+  SERIALIZATION_ERROR,
+  SEND_REQUEST_TO_REMOTE,
+  RETRIEVE_RESPONSE,
+  DESERIALIZATION_ERROR
+
+}
