11c1af5d5957e3640fbd49151600cf60e620dde3
dankito <dankito@user.noreply.github.com>
2017-02-08 16:50:55 +0100

    Implemented SocketHandler

---
+++ libs/TcpSocketClientCommunicator/src/main/java/net/dankito/sync/communication/SocketHandler.java
@@ -0,0 +1,97 @@
+package net.dankito.sync.communication;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+
+public class SocketHandler {
+
+  private static final Logger log = LoggerFactory.getLogger(SocketHandler.class);
+
+
+  public SocketResult sendMessage(Socket socket, byte[] message) {
+    InputStream inputStream = null;
+    OutputStream outputStream = null;
+
+    try {
+      inputStream = new ByteArrayInputStream(message);
+      outputStream = socket.getOutputStream();
+
+      return sendMessage(inputStream, outputStream);
+    }
+    catch(Exception e) {
+      log.error("Could not send message to client " + (socket != null ? socket.getInetAddress() : ""), e);
+      return new SocketResult(e);
+    }
+    finally {
+      if(inputStream != null) {
+        try { inputStream.close(); } catch(Exception e) { log.warn("Could not close input stream", e); }
+      }
+
+      if(outputStream != null) {
+        try { outputStream.flush(); } catch(Exception e) { log.warn("Could not flush output stream", e); }
+        // do not close outputStream, otherwise socket gets closed as well
+      }
+    }
+  }
+
+  protected SocketResult sendMessage(InputStream inputStream, OutputStream outputStream) throws IOException {
+    byte[] buffer = new byte[CommunicationConfig.BUFFER_SIZE];
+    int read;
+
+    while((read = inputStream.read(buffer, 0, buffer.length)) > 0) {
+      outputStream.write(buffer, 0, read);
+    }
+
+    return new SocketResult(true);
+  }
+
+
+  public SocketResult receiveMessage(Socket socket) {
+    try {
+      InputStream inputStream = socket.getInputStream();
+
+      // do not close inputStream, otherwise socket gets closed
+      return receiveMessage(inputStream);
+    } catch(Exception e) {
+      log.error("Could not receive response", e);
+
+      return new SocketResult(e);
+    }
+  }
+
+  protected SocketResult receiveMessage(InputStream inputStream) throws IOException {
+    byte[] buffer = new byte[CommunicationConfig.MAX_MESSAGE_SIZE];
+    int read = inputStream.read(buffer, 0, buffer.length);
+
+    if(read > -1 && read < CommunicationConfig.MAX_MESSAGE_SIZE) {
+      String responseString = new String(buffer, 0, read, CommunicationConfig.MESSAGE_CHARSET_NAME);
+      buffer = null; // TODO: run GarbageCollector to free memory?
+
+      return new SocketResult(responseString);
+    }
+    else {
+      if(read <= 0) {
+        return new SocketResult(new Exception("Could not receive any bytes"));
+      }
+      else {
+        return new SocketResult(new Exception("Received message exceeds max message length of " + CommunicationConfig.MAX_MESSAGE_SIZE));
+      }
+    }
+  }
+
+  public void closeSocket(Socket socket) {
+    if(socket != null) {
+      try {
+        socket.close();
+      } catch(Exception e) { log.error("Could not close socket", e); }
+    }
+  }
+
+}
---
+++ libs/TcpSocketClientCommunicator/src/main/java/net/dankito/sync/communication/SocketResult.java
@@ -0,0 +1,56 @@
+package net.dankito.sync.communication;
+
+
+public class SocketResult {
+
+  protected boolean isSuccessful;
+
+  protected Exception error;
+
+  protected String receivedMessage;
+
+
+  public SocketResult(boolean isSuccessful) {
+    this.isSuccessful = isSuccessful;
+  }
+
+  public SocketResult(Exception error) {
+    this(false);
+    this.error = error;
+  }
+
+  public SocketResult(String receivedMessage) {
+    this(true);
+    this.receivedMessage = receivedMessage;
+  }
+
+
+  public boolean isSuccessful() {
+    return isSuccessful;
+  }
+
+  public Exception getError() {
+    return error;
+  }
+
+  public String getReceivedMessage() {
+    return receivedMessage;
+  }
+
+
+  @Override
+  public String toString() {
+    if(isSuccessful()) {
+      if(getReceivedMessage() != null) {
+        return "Successful: " + getReceivedMessage();
+      }
+      else {
+        return "Successful";
+      }
+    }
+    else {
+      return "Error: " + getError();
+    }
+  }
+
+}
