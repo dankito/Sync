04f0ce8d5552e769859743d84c83239e92beefc9
dankito <dankito@user.noreply.github.com>
2017-02-10 16:56:09 +0100

    Implemented mapping response

--- SyncCommonLogic/src/main/java/net/dankito/sync/communication/TcpSocketClientCommunicator.java
+++ SyncCommonLogic/src/main/java/net/dankito/sync/communication/TcpSocketClientCommunicator.java
@@ -1,11 +1,12 @@
 package net.dankito.sync.communication;
 
+import net.dankito.communication.IRequestSender;
+import net.dankito.communication.message.Request;
+import net.dankito.communication.message.Response;
+import net.dankito.communication.message.ResponseErrorType;
 import net.dankito.sync.communication.callback.SendRequestCallback;
 import net.dankito.sync.communication.message.DeviceInfo;
-import net.dankito.sync.communication.message.MessageHandler;
-import net.dankito.sync.communication.message.Request;
 import net.dankito.sync.devices.DiscoveredDevice;
-import net.dankito.utils.IThreadPool;
 
 import java.net.InetSocketAddress;
 
@@ -23,7 +24,26 @@ public class TcpSocketClientCommunicator implements IClientCommunicator {
   @Override
   public void getDeviceInfo(DiscoveredDevice remoteDevice, final SendRequestCallback<DeviceInfo> callback) {
     requestSender.sendRequestAndReceiveResponseAsync(new InetSocketAddress(remoteDevice.getAddress(), remoteDevice.getMessagesPort()),
-        new Request(CommunicationConfig.GET_DEVICE_INFO_METHOD_NAME), callback);
+        new Request(CommunicatorConfig.GET_DEVICE_INFO_METHOD_NAME), new net.dankito.communication.callback.SendRequestCallback<DeviceInfo>() {
+          @Override
+          public void done(Response<DeviceInfo> response) {
+            callback.done(mapResponse(response));
+          }
+        });
+  }
+
+
+  protected net.dankito.sync.communication.message.Response mapResponse(Response response) {
+    return new net.dankito.sync.communication.message.Response(response.isCouldHandleMessage(), mapResponseErrorType(response.getErrorType()),
+        response.getError(), response.getBody());
+  }
+
+  protected net.dankito.sync.communication.message.ResponseErrorType mapResponseErrorType(ResponseErrorType errorType) {
+    if(errorType != null) {
+      return net.dankito.sync.communication.message.ResponseErrorType.valueOf(errorType.name());
+    }
+
+    return null;
   }
 
 }
