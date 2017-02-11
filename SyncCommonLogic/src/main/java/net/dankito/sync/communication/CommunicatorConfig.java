322a154b36de4b42a0ab2f4a6ee31a211dcab048
dankito <dankito@user.noreply.github.com>
2017-02-11 09:08:01 +0100

    Added REQUEST_START_SYNCHRONIZATION_METHOD_NAME

--- SyncCommonLogic/src/main/java/net/dankito/sync/communication/CommunicatorConfig.java
+++ SyncCommonLogic/src/main/java/net/dankito/sync/communication/CommunicatorConfig.java
@@ -8,4 +8,6 @@ public class CommunicatorConfig {
 
   public static final String GET_DEVICE_INFO_METHOD_NAME = "GetDeviceInfo";
 
+  public static final String REQUEST_START_SYNCHRONIZATION_METHOD_NAME = "RequestStartSynchronization";
+
 }
