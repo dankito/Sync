3f4657549c06a767f5dfc5f8d56889a51b8c58e2
dankito <dankito@user.noreply.github.com>
2017-02-11 09:01:23 +0100

    Added comment

--- SyncCommonLogic/src/main/java/net/dankito/sync/communication/callback/RequestHandlerCallback.java
+++ SyncCommonLogic/src/main/java/net/dankito/sync/communication/callback/RequestHandlerCallback.java
@@ -5,6 +5,6 @@ import net.dankito.communication.message.Response;
 
 public interface RequestHandlerCallback {
 
-  void done(Response response);
+  void done(Response response); // TODO: it should actually return a net.dankito.sync.communication.message.Response to keep layer consistency, but would needed double mapping
 
 }
