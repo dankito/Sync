e96c1013a1adb41167fff57ced592bb0404fb473
dankito <dankito@user.noreply.github.com>
2017-02-11 09:10:27 +0100

    Made class ready for dependency injection

--- SyncCommonLogic/src/main/java/net/dankito/sync/communication/message/MessageHandlerConfig.java
+++ SyncCommonLogic/src/main/java/net/dankito/sync/communication/message/MessageHandlerConfig.java
@@ -2,8 +2,13 @@ package net.dankito.sync.communication.message;
 
 import net.dankito.sync.communication.IRequestHandler;
 import net.dankito.sync.devices.INetworkSettings;
+import net.dankito.sync.synchronization.ISyncManager;
+
+import javax.inject.Inject;
+import javax.inject.Named;
 
 
+@Named
 public class MessageHandlerConfig {
 
   protected INetworkSettings networkSettings;
@@ -14,6 +19,11 @@ public class MessageHandlerConfig {
   public MessageHandlerConfig(INetworkSettings networkSettings, IRequestHandler requestStartSynchronizationHandler) {
     this.networkSettings = networkSettings;
     this.requestStartSynchronizationHandler = requestStartSynchronizationHandler;
+  }
+
+  @Inject
+  public MessageHandlerConfig(INetworkSettings networkSettings, ISyncManager syncManager) {
+    this(networkSettings, syncManager.getRequestStartSynchronizationHandler());
   }
 
 
