8457c7d622774e9d968721fb7b5ce77f8abc2fb7
dankito <dankito@user.noreply.github.com>
2017-02-11 15:40:04 +0100

    Adjusted port settings

--- SyncCommonLogic/src/main/java/net/dankito/sync/communication/CommunicatorConfig.java
+++ SyncCommonLogic/src/main/java/net/dankito/sync/communication/CommunicatorConfig.java
@@ -1,9 +1,11 @@
 package net.dankito.sync.communication;
 
+import net.dankito.sync.devices.DevicesManagerConfig;
+
 
 public class CommunicatorConfig {
 
-  public static final int DEFAULT_MESSAGES_RECEIVER_PORT = 27388;
+  public static final int DEFAULT_MESSAGES_RECEIVER_PORT = DevicesManagerConfig.DEVICES_DISCOVERER_PORT + 1;
 
 
   public static final String GET_DEVICE_INFO_METHOD_NAME = "GetDeviceInfo";
--- SyncCommonLogic/src/main/java/net/dankito/sync/synchronization/SynchronizationConfig.java
+++ SyncCommonLogic/src/main/java/net/dankito/sync/synchronization/SynchronizationConfig.java
@@ -1,9 +1,11 @@
 package net.dankito.sync.synchronization;
 
 
+import net.dankito.sync.devices.DevicesManagerConfig;
+
 public class SynchronizationConfig {
 
-  public static final int DEFAULT_SYNCHRONIZATION_PORT = 32788;
+  public static final int DEFAULT_SYNCHRONIZATION_PORT = DevicesManagerConfig.DEVICES_DISCOVERER_PORT + 2;
 
   public static final boolean DEFAULT_ALSO_USE_PULL_REPLICATION = false;
 
