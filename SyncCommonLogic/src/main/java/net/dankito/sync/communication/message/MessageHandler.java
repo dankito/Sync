08a213729d5347e09fa0bfe802d27f04244aa444
dankito <dankito@user.noreply.github.com>
2017-02-11 09:11:13 +0100

    Imiplemented handling REQUEST_START_SYNCHRONIZATION

--- SyncCommonLogic/src/main/java/net/dankito/sync/communication/message/MessageHandler.java
+++ SyncCommonLogic/src/main/java/net/dankito/sync/communication/message/MessageHandler.java
@@ -1,56 +1,68 @@
 package net.dankito.sync.communication.message;
 
 import net.dankito.communication.IMessageHandler;
+import net.dankito.communication.callback.RequestHandlerCallback;
 import net.dankito.communication.message.Request;
 import net.dankito.communication.message.Response;
-import net.dankito.sync.Device;
 import net.dankito.sync.communication.CommunicatorConfig;
 
 
 public class MessageHandler implements IMessageHandler {
 
-  protected Device localDevice;
+  protected MessageHandlerConfig config;
 
 
-  public MessageHandler(Device localDevice) {
-    this.localDevice = localDevice;
+  public MessageHandler(MessageHandlerConfig config) {
+    this.config = config;
   }
 
 
   @Override
-  public Response handleReceivedRequest(Request request) {
+  public void handleReceivedRequest(Request request, RequestHandlerCallback callback) {
     switch(request.getMethod()) {
       case CommunicatorConfig.GET_DEVICE_INFO_METHOD_NAME:
-        return handleGetDeviceInfoRequest(request);
-    }
-
-    return null;
-  }
-
-
-  @Override
-  public Class getResponseBodyClassForMethod(String methodName) throws Exception {
-    switch(methodName) {
-      case CommunicatorConfig.GET_DEVICE_INFO_METHOD_NAME:
-        return DeviceInfo.class;
-      default:
-        throw new Exception("Don't know how to deserialize response of method " + methodName); // TODO: translate
+        callback.done(handleGetDeviceInfoRequest(request));
+      case CommunicatorConfig.REQUEST_START_SYNCHRONIZATION_METHOD_NAME:
+        handleRequestStartSynchronizationRequest(request, callback);
     }
   }
+
+
+  protected Response handleGetDeviceInfoRequest(Request request) {
+    return new Response(DeviceInfo.fromDevice(config.getNetworkSettings().getLocalHostDevice()));
+  }
+
+  protected void handleRequestStartSynchronizationRequest(Request request, final RequestHandlerCallback callback) {
+    config.getRequestStartSynchronizationHandler().handle(request, new net.dankito.sync.communication.callback.RequestHandlerCallback() {
+      @Override
+      public void done(Response response) {
+        callback.done(response);
+      }
+    });
+  }
+
 
   @Override
   public Class getRequestBodyClassForMethod(String methodName) throws Exception {
     switch(methodName) {
       case CommunicatorConfig.GET_DEVICE_INFO_METHOD_NAME:
+      case CommunicatorConfig.REQUEST_START_SYNCHRONIZATION_METHOD_NAME:
         return null; // requests without request bodies
       default:
         throw new Exception("Don't know how to deserialize response of method " + methodName); // TODO: translate
     }
   }
 
-
-  protected Response handleGetDeviceInfoRequest(Request request) {
-    return new Response(DeviceInfo.fromDevice(localDevice));
+  @Override
+  public Class getResponseBodyClassForMethod(String methodName) throws Exception {
+    switch(methodName) {
+      case CommunicatorConfig.GET_DEVICE_INFO_METHOD_NAME:
+        return DeviceInfo.class;
+      case CommunicatorConfig.REQUEST_START_SYNCHRONIZATION_METHOD_NAME:
+        return RequestStartSynchronizationResponseBody.class;
+      default:
+        throw new Exception("Don't know how to deserialize response of method " + methodName); // TODO: translate
+    }
   }
 
 }
