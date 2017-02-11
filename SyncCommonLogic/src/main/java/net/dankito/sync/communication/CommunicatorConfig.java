8a7df88d0eeec04e963b2f1d1e89aeff5193666d
dankito <dankito@user.noreply.github.com>
2017-02-11 14:06:40 +0100

    Added REQUEST_PERMIT_SYNCHRONIZATION_METHOD_NAME and RESPONSE_TO_SYNCHRONIZATION_PERMITTING_CHALLENGE_METHOD_NAME

--- SyncCommonLogic/src/main/java/net/dankito/sync/communication/CommunicatorConfig.java
+++ SyncCommonLogic/src/main/java/net/dankito/sync/communication/CommunicatorConfig.java
@@ -8,6 +8,10 @@ public class CommunicatorConfig {
 
   public static final String GET_DEVICE_INFO_METHOD_NAME = "GetDeviceInfo";
 
+  public static final String REQUEST_PERMIT_SYNCHRONIZATION_METHOD_NAME = "RequestPermitSynchronization";
+
+  public static final String RESPONSE_TO_SYNCHRONIZATION_PERMITTING_CHALLENGE_METHOD_NAME = "ResponseToSynchronizationPermittingChallenge";
+
   public static final String REQUEST_START_SYNCHRONIZATION_METHOD_NAME = "RequestStartSynchronization";
 
 }
