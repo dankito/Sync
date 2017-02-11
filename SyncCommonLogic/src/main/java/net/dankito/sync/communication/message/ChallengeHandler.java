d68121ecec3bfc196a3f0b656199f577274f1e2b
dankito <dankito@user.noreply.github.com>
2017-02-11 21:41:53 +0100

    Implemented createChallengeResponse()

--- SyncCommonLogic/src/main/java/net/dankito/sync/communication/message/ChallengeHandler.java
+++ SyncCommonLogic/src/main/java/net/dankito/sync/communication/message/ChallengeHandler.java
@@ -1,6 +1,9 @@
 package net.dankito.sync.communication.message;
 
 
+import net.dankito.utils.services.HashAlgorithm;
+import net.dankito.utils.services.HashService;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -9,6 +12,10 @@ public class ChallengeHandler {
 
   protected static final int DEFAULT_COUNT_RETRIES = 2;
 
+  protected static final HashAlgorithm CHALLENGE_RESPONSE_HASH_ALGORITHM = HashAlgorithm.SHA512;
+
+
+  protected HashService hashService = new HashService();
 
   protected Map<String, DeviceInfo> nonceToDeviceInfoMap = new HashMap<>();
 
@@ -35,9 +42,19 @@ public class ChallengeHandler {
   }
 
 
+  public String createChallengeResponse(String nonce, String enteredCode) {
+    String challengeResponse = nonce + "-" + enteredCode;
+
+    try {
+      return hashService.hashString(CHALLENGE_RESPONSE_HASH_ALGORITHM, challengeResponse);
+    } catch(Exception e) { /* should actually never occur */ }
+
+    return null;
+  }
+
+
   public boolean isResponseOk(String nonce, String challengeResponse) {
-    // check if nonceToCorrectResponsesMap really contains nonce as otherwise (null, null) would be a correct response
-    boolean isCorrectResponse = nonceToCorrectResponsesMap.containsKey(nonce) && challengeResponse.equals(nonceToCorrectResponsesMap.get(nonce));
+    boolean isCorrectResponse = isCorrectResponse(nonce, challengeResponse);
 
     if(isCorrectResponse) {
       nonceToCountRetriesMap.remove(nonce);
@@ -55,6 +72,18 @@ public class ChallengeHandler {
     }
 
     return isCorrectResponse;
+  }
+
+  protected boolean isCorrectResponse(String nonce, String challengeResponse) {
+    // check if nonceToCorrectResponsesMap really contains nonce as otherwise (null, null) would be a correct response
+    if(nonceToCorrectResponsesMap.containsKey(nonce)) {
+      String correctResponse = nonceToCorrectResponsesMap.get(nonce);
+      String correctChallengeResponse = createChallengeResponse(nonce, correctResponse);
+
+      challengeResponse.equals(correctChallengeResponse);
+    }
+
+    return false;
   }
 
   public int getCountRetriesLeftForNonce(String nonce) {
