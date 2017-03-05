3fd4ca563a4142867568a8a4ee07236e46e69636
dankito <dankito@user.noreply.github.com>
2017-03-05 23:11:11 +0100

    Added generateUuid()

--- thunderbird-plugin/plugin/chrome/content/util.js
+++ thunderbird-plugin/plugin/chrome/content/util.js
@@ -14,3 +14,11 @@ function objectToJson(object) {
 function stringStartsWith(string, startString) {
     return string.lastIndexOf(startString, 0) === 0;
 }
+
+// copied from https://stackoverflow.com/a/2117523
+function generateUuid() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+        return v.toString(16);
+    });
+}
