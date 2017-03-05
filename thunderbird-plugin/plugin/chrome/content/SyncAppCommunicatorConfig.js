936f0e7ab1e331264d5bd2e89e27662075797048
dankito <dankito@user.noreply.github.com>
2017-03-05 20:40:13 +0100

    Added SyncAppCommunicatorConfig

---
+++ thunderbird-plugin/plugin/chrome/content/SyncAppCommunicatorConfig.js
@@ -0,0 +1,9 @@
+var SyncAppCommunicatorConfig = new function() {
+
+    this.DevicesDiscovererUdpPort = 32788;
+
+    this.SendDevicesDiscovererMessageIntervalMillis = 500;
+
+    this.MessagesReceiverPort = 32797;
+
+}
