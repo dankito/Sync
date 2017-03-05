7d86c6442f7e43dd996d38dfe6c919b1edeb49b9
dankito <dankito@user.noreply.github.com>
2017-03-05 20:39:34 +0100

    Added logObject() and objectToJson()

--- thunderbird-plugin/plugin/chrome/content/util.js
+++ thunderbird-plugin/plugin/chrome/content/util.js
@@ -2,3 +2,11 @@
 function log(message) {
     Application.console.log(message);
 }
+
+function logObject(object) {
+    log(objectToJson(object));
+}
+
+function objectToJson(object) {
+    return JSON.stringify(object);
+}
