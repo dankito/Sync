fac388120985328400663d5d9c29260073742e9c
dankito <dankito@user.noreply.github.com>
2017-03-05 11:04:24 +0100

    Made plugin executing code in Thunderbird

---
+++ thunderbird-plugin/plugin/chrome/content/sync.js
@@ -0,0 +1,30 @@
+
+function log(message) {
+    Application.console.log(message);
+}
+
+
+window.addEventListener("load", function(e) {
+    log('Loaded Thunderbird')
+	syncAddressBook();
+}, false);
+
+
+function syncAddressBook() {
+    log('Starting to sync address book ...')
+
+    let abManager = Components.classes["@mozilla.org/abmanager;1"]
+                              .getService(Components.interfaces.nsIAbManager);
+
+    let allAddressBooks = abManager.directories;
+
+    while (allAddressBooks.hasMoreElements()) {
+      let addressBook = allAddressBooks.getNext()
+                                       .QueryInterface(Components.interfaces.nsIAbDirectory);
+      if (addressBook instanceof Components.interfaces.nsIAbDirectory) { // or nsIAbItem or nsIAbCollection
+        log("Directory Name:" + addressBook.dirName);
+      }
+    }
+}
+
+log('started (i feels so good to be back to JavaScript');
