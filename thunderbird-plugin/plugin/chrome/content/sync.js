88e69107ab0dbe4d198509ecfaa65825a8bd57e4
dankito <dankito@user.noreply.github.com>
2017-03-05 11:35:47 +0100

    Added network.js with a first implementation for listening for incoming socket messages

--- thunderbird-plugin/plugin/chrome/content/sync.js
+++ thunderbird-plugin/plugin/chrome/content/sync.js
@@ -1,9 +1,11 @@
 
 
 window.addEventListener("load", function(e) {
-    log('Loaded Thunderbird')
 	syncAddressBook();
 }, false);
 
 
-log('started (i feels so good to be back to JavaScript)');
+log('Started Sync Thunderbird Plugin');
+
+
+DevicesDiscovererListener.start();
