43a439f1c8f86245a97169d064642d5c141f36d7
dankito <dankito@user.noreply.github.com>
2017-03-05 18:30:56 +0100

    Implemented sendMessageViaTcp() and sendMessageViaUdp()

--- thunderbird-plugin/plugin/chrome/content/network.js
+++ thunderbird-plugin/plugin/chrome/content/network.js
@@ -10,17 +10,20 @@ var DevicesDiscovererListener = new function () {
             var output = transport.openOutputStream(Ci.nsITransport.OPEN_BLOCKING, 0, 0);
             var sin = Cc["@mozilla.org/scriptableinputstream;1"].createInstance(Ci.nsIScriptableInputStream);
 
-            try{
+            try {
                 sin.init(input);
-                var readBytes = sin.available();
 
-                var request = '';
-                request = sin.read(readBytes);
-                log('Received: ' + request);
+                var received = '';
+                while (sin.available()) {
+                    received = received + sin.read(512);
+                }
 
-                getUrl(request);
+                log('Received: ' + received);
 
-                output.write("yes", "yes".length);
+                var contacts = getAllContacts();
+                var contactsJson = JSON.stringify(contacts);
+
+                output.write(contactsJson, contactsJson.length);
                 output.flush();
             }
             finally{
@@ -35,9 +38,72 @@ var DevicesDiscovererListener = new function () {
      * Main
      */
     this.start = function() {
+        var messagesPort = 32797;
+
         var serverSocket = Cc["@mozilla.org/network/server-socket;1"].createInstance(Ci.nsIServerSocket);
-        serverSocket.init(8888, false, 5);
+        serverSocket.init(messagesPort, false, 5);
         log("Opened socket on " + serverSocket.port);
         serverSocket.asyncListen(listener);
+    };
+
+
+    /**
+        Supports sending a message via UDP, but only to a single peer.
+        Broadcast (e.g. to 192.168.0.255) is not supported!
+    */
+    this.sendMessageViaUdp = function(address, port, message) {
+        var types = new Array();
+        types.push('udp');
+
+        var udpSocket = Components.classes["@mozilla.org/network/socket-transport-service;1"]
+                                          .getService(Components.interfaces.nsISocketTransportService)
+                                          .createTransport(types, types.length, address, port, null);
+
+        var outputStream = udpSocket.openOutputStream(0, 0, 0);
+
+        outputStream.write(message, message.length);
+
+        outputStream.flush();
+    };
+
+
+    this.sendMessageViaTcp = function(address, port, message) {
+        var socket = Components.classes["@mozilla.org/network/socket-transport-service;1"]
+                                  .getService(Components.interfaces.nsISocketTransportService)
+                                  .createTransport(null, 0, address, port, null);
+
+        var poolOutputStream = socket.openOutputStream(0, 0, 0);
+
+        poolOutputStream.write(message, message.length);
+
+        var poolRawInputStream = socket.openInputStream(0, 0, 0);
+        var poolInputStream = Components.classes ["@mozilla.org/scriptableinputstream;1"]
+                                .createInstance(Components.interfaces.nsIScriptableInputStream)
+                                .init(poolRawInputStream);
+
+        Components.utils.import("resource://gre/modules/NetUtil.jsm");
+
+        var pump = Components.classes["@mozilla.org/network/input-stream-pump;1"]
+                             .createInstance(Components.interfaces.nsIInputStreamPump);
+        pump.init(poolRawInputStream, -1, -1, 0, 0, true);
+
+        var listener = {
+          onStartRequest: function(request, context) {},
+          onDataAvailable: function(request, context, stream, offset, count)
+          {
+            var data = NetUtil.readInputStreamToString(stream, count);
+            log('Received data in onDataAvailable():');
+            log(data);
+            // TODO: call listener to return received message
+          },
+          onStopRequest: function(request, context, result)
+          {
+            if (!Components.isSuccessCode(result)) {
+              // TODO: Handle error here
+            }
+          }
+        };
+
+        pump.asyncRead(listener, null);
     };
 }
