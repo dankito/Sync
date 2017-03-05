2667d85a9df7c004434700c202b6d57ce4c4e033
dankito <dankito@user.noreply.github.com>
2017-03-05 18:35:51 +0100

    Implemented getAllContacts()

--- thunderbird-plugin/plugin/chrome/content/addressbook.js
+++ thunderbird-plugin/plugin/chrome/content/addressbook.js
@@ -1,10 +1,19 @@
 
 
 function syncAddressBook() {
-    log('Starting to sync address book ...')
+    log('Starting to sync address book ...');
+
+    var contacts = getAllContacts();
+}
+
+
+function getAllContacts() {
+    log('Getting all contacts ...');
+
+    var contacts = new Array();
 
     let abManager = Components.classes["@mozilla.org/abmanager;1"]
-                              .getService(Components.interfaces.nsIAbManager);
+                                  .getService(Components.interfaces.nsIAbManager);
 
     let allAddressBooks = abManager.directories;
 
@@ -12,7 +21,99 @@ function syncAddressBook() {
       let addressBook = allAddressBooks.getNext()
                                        .QueryInterface(Components.interfaces.nsIAbDirectory);
       if (addressBook instanceof Components.interfaces.nsIAbDirectory) { // or nsIAbItem or nsIAbCollection
-        log("Directory Name:" + addressBook.dirName);
+        log("AddressBook Name:" + addressBook.dirName);
+        var contactsInAddressBooks = getAllContactsFromAddressBook(addressBook);
+        var nextContact;
+
+        while(nextContact = GetNextValidContact(contactsInAddressBooks)) {
+            contacts.push(nextContact);
+        }
       }
     }
+
+    log('Retrieved ' + contacts.length + ' contacts from address book');
+
+    return contacts;
+}
+
+
+
+function getAllContactsFromAddressBook(addressbook) {
+	var contacts = addressbook.childCards;
+
+	try {
+		contacts.hasMoreElements();
+	}
+	catch(ex) {
+		try {
+			contacts.first();
+		}
+		catch(ex) {}
+
+		var cse = new CardsSimpleEnum();
+
+		cse.enumerator = contacts;
+
+		return cse;
+	}
+
+	return contacts;
+}
+
+
+function CardsSimpleEnum() {};
+
+CardsSimpleEnum.prototype = {
+	buffer : null,
+	atend : false,
+	enumerator : null,
+
+	hasMoreElements : function () {
+		if(this.buffer != null) return true;
+
+		if(this.atend) return false;
+
+		try {
+			this.buffer = this.enumerator.currentItem();
+
+			try {
+				this.enumerator.next();
+			}
+			catch (ex) {
+				this.atend = true;
+			}
+
+			return true;
+		}
+		catch (ex) {}
+
+		return false;
+	},
+
+	getNext : function () {
+		if(this.buffer != null) {
+			var tmp = this.buffer;
+			this.buffer = null;
+			return tmp;
+		}
+		else {
+			throw("hasMoreElements() was not called");
+		}
+	}
+};
+
+
+function GetNextValidContact(contacts) {
+	var contact = null;
+
+	while(contacts.hasMoreElements()) {
+		contact = contacts.getNext();
+		contact = contact.QueryInterface(Components.interfaces.nsIAbCard);
+
+		if (contact.isMailList) {
+			contact = null;
+		}
+	}
+
+	return contact;
 }
