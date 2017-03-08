dae022f7e05b5c04f2e60f2aa27787efb9a19a84
dankito <dankito@user.noreply.github.com>
2017-03-08 23:01:56 +0100

    Added SYNC_CONTACT_MESSAGE

--- SyncCommonLogic/src/main/java/net/dankito/sync/thunderbird/ThunderbirdMessageConfig.java
+++ SyncCommonLogic/src/main/java/net/dankito/sync/thunderbird/ThunderbirdMessageConfig.java
@@ -8,4 +8,6 @@ public class ThunderbirdMessageConfig {
   public static final Charset MESSAGES_CHARSET = Charset.forName("UTF-8");
 
   public static final String GET_ADDRESS_BOOK_MESSAGE = "GET_ADDRESS_BOOK";
+
+  public static final String SYNC_CONTACT_MESSAGE = "SYNC_CONTACT";
 }
