88d2506af3407ce46f3d502b38eaa16dc4a0ec7c
dankito <dankito@user.noreply.github.com>
2017-03-08 23:53:33 +0100

    Started ThunderbirdContactsSyncModule

---
+++ SyncCommonLogic/src/main/java/net/dankito/sync/synchronization/modules/ThunderbirdContactsSyncModule.java
@@ -0,0 +1,75 @@
+package net.dankito.sync.synchronization.modules;
+
+import net.dankito.sync.ContactSyncEntity;
+import net.dankito.sync.SyncEntityState;
+import net.dankito.sync.SyncJobItem;
+import net.dankito.sync.devices.DiscoveredDevice;
+import net.dankito.sync.localization.Localization;
+import net.dankito.sync.synchronization.SyncEntityChange;
+import net.dankito.sync.synchronization.SyncEntityChangeListener;
+import net.dankito.sync.thunderbird.ThunderbirdPluginConnector;
+import net.dankito.sync.thunderbird.callback.ThunderbirdCallback;
+import net.dankito.sync.thunderbird.response.GetAddressBookResponse;
+import net.dankito.utils.IThreadPool;
+
+import java.util.ArrayList;
+
+
+public class ThunderbirdContactsSyncModule extends SyncModuleBase {
+
+  protected DiscoveredDevice thunderbird;
+
+  protected ThunderbirdPluginConnector connector;
+
+
+  public ThunderbirdContactsSyncModule(DiscoveredDevice thunderbird, Localization localization, IThreadPool threadPool) {
+    super(localization);
+
+    this.thunderbird = thunderbird;
+    this.connector = new ThunderbirdPluginConnector(thunderbird, threadPool, thunderbirdEntityChangeListener);
+  }
+
+  @Override
+  protected String getNameStringResourceKey() {
+    return "sync.module.name.contacts";
+  }
+
+  @Override
+  public int getDisplayPriority() {
+    return DISPLAY_PRIORITY_HIGH;
+  }
+
+  @Override
+  public String getSyncEntityTypeItCanHandle() {
+    return SyncModuleDefaultTypes.CONTACTS.getTypeName();
+  }
+
+  @Override
+  public void readAllEntitiesAsync(final ReadEntitiesCallback callback) {
+    connector.getAddressBookAsync(new ThunderbirdCallback<GetAddressBookResponse>() {
+      @Override
+      public void done(GetAddressBookResponse response) {
+        if(response.isCouldHandleMessage()) {
+          callback.done(response.getBody());
+        }
+        else {
+          callback.done(new ArrayList<ContactSyncEntity>()); // TODO: inform caller that an error occured
+        }
+      }
+    });
+  }
+
+  @Override
+  public void handleRetrievedSynchronizedEntityAsync(SyncJobItem jobItem, SyncEntityState entityState, HandleRetrievedSynchronizedEntityCallback callback) {
+
+  }
+
+
+  protected SyncEntityChangeListener thunderbirdEntityChangeListener = new SyncEntityChangeListener() {
+    @Override
+    public void entityChanged(SyncEntityChange change) {
+      callSyncEntityChangeListeners(new SyncEntityChange(ThunderbirdContactsSyncModule.this, change.getSyncEntity()));
+    }
+  };
+
+}
