8aca4f940af1c2954e1365163e46a343412dd81d
dankito <dankito@user.noreply.github.com>
2017-03-08 23:21:04 +0100

    Passing received contacts on to parent via ContactSynchronizedListener

--- SyncCommonLogic/src/main/java/net/dankito/sync/thunderbird/ThunderbirdMessageHandler.java
+++ SyncCommonLogic/src/main/java/net/dankito/sync/thunderbird/ThunderbirdMessageHandler.java
@@ -3,11 +3,20 @@ package net.dankito.sync.thunderbird;
 import net.dankito.communication.IMessageHandler;
 import net.dankito.communication.callback.RequestHandlerCallback;
 import net.dankito.communication.message.Request;
+import net.dankito.sync.thunderbird.callback.ContactSynchronizedListener;
 import net.dankito.sync.thunderbird.model.ContactSync;
 import net.dankito.sync.thunderbird.model.ThunderbirdContact;
 
 
 public class ThunderbirdMessageHandler implements IMessageHandler {
+
+  protected ContactSynchronizedListener contactSynchronizedListener = null;
+
+
+  public ThunderbirdMessageHandler(ContactSynchronizedListener contactSynchronizedListener) {
+    this.contactSynchronizedListener = contactSynchronizedListener;
+  }
+
 
   @Override
   public void handleReceivedRequest(Request request, RequestHandlerCallback callback) {
@@ -19,7 +28,13 @@ public class ThunderbirdMessageHandler implements IMessageHandler {
   }
 
   protected void handleSynchronizedContact(Request request) {
+    if(request.isBodySet()) {
+      ContactSync contactSync = (ContactSync)request.getBody();
 
+      if(contactSynchronizedListener != null) {
+        contactSynchronizedListener.contactSynchronized(contactSync.getState(), contactSync.getContact());
+      }
+    }
   }
 
   @Override
