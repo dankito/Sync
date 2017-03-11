fa89de887fc4b0faa43f26bb411f0f8a5022be17
dankito <dankito@user.noreply.github.com>
2017-03-12 00:23:18 +0100

    Implemented mapSyncContactToCard()

--- thunderbird-plugin/plugin/chrome/content/ContactHandler.js
+++ thunderbird-plugin/plugin/chrome/content/ContactHandler.js
@@ -37,4 +37,25 @@ var ContactHandler = new function() {
 
         return mapped;
     };
-}
+
+    this.mapSyncContactToCard = function(contact) {
+        let card = Components.classes["@mozilla.org/addressbook/cardproperty;1"].createInstance(Components.interfaces.nsIAbCard);
+
+        card.uuid = contact.uuid;
+        card.localId = contact.localId;
+
+        try {
+            Object.keys(contact).forEach(function(property) {
+                try {
+                    let value = contact[property];
+                    if(value != null) {
+                        card.setProperty(property, value);
+                    }
+                } catch(e) { log('Could not set property ' + property + ' on card:'); log(e); }
+            });
+        } catch(e) { log('Could not map all properties of Sync contact to card'); log(e); }
+
+        return card;
+    };
+
+};
