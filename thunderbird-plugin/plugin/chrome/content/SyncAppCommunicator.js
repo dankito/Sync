920eddf440497de638e1a49f643392d53db3f068
dankito <dankito@user.noreply.github.com>
2017-03-12 11:10:02 +0100

    Implemented _respondToRequestPermitToSynchronize() and _respondToResponseToSynchronizationPermittingChallenge

--- thunderbird-plugin/plugin/chrome/content/SyncAppCommunicator.js
+++ thunderbird-plugin/plugin/chrome/content/SyncAppCommunicator.js
@@ -35,6 +35,14 @@ var SyncAppCommunicator = new function () {
                 'synchronizationPort' : SyncAppCommunicatorConfig.DeviceDoesNotSupportActiveSynchronization
             };
         }
+
+        else if(stringStartsWith(receivedMessage, SyncAppCommunicatorConfig.RequestPermitSynchronizationMessage)) {
+            return _respondToRequestPermitToSynchronize(_getMessageBody(receivedMessage));
+        }
+        else if(stringStartsWith(receivedMessage, SyncAppCommunicatorConfig.ResponseToSynchronizationPermittingChallengeMessage)) {
+            return _respondToResponseToSynchronizationPermittingChallenge(_getMessageBody(receivedMessage));
+        }
+
         else if(stringStartsWith(receivedMessage, SyncAppCommunicatorConfig.GetAddressBookMessage)) {
             return AddressBook.getAllContacts();
         }
@@ -43,9 +51,86 @@ var SyncAppCommunicator = new function () {
         }
     };
 
+
+    var _respondToRequestPermitToSynchronize = function(deviceInfo) {
+        try { logObject(deviceInfo); } catch(e) { }
+
+        var deviceInfoString = deviceInfo.osName + ' ' + deviceInfo.osVersion;
+        if(deviceInfo.name) {
+            deviceInfoString = deviceInfo.name + ' ' + deviceInfoString;
+        }
+
+        var result = confirm('Allow device ' + deviceInfoString + ' to synchronize?'); // TODO: translate
+
+        if(result) {
+            return _createCorrectResponseAndReturnRespondToChallenge();
+        }
+        else {
+            return {
+                'result' : 'DENIED',
+                'nonce' : null
+            }
+        }
+    };
+
+    var _nonceToCorrectResponseMap = { };
+    var _nonceToCountRetriesLeftMap = { };
+
+    function _createCorrectResponseAndReturnRespondToChallenge() {
+        var nonce = generateUuid();
+        var codeToEnter = '' + Math.floor((Math.random() * 1000000));
+
+        var correctResponse = nonce + "-" + codeToEnter;
+        correctResponse = createSha512HashAsBase64(correctResponse);
+
+        _nonceToCorrectResponseMap[nonce] = correctResponse;
+        _nonceToCountRetriesLeftMap[nonce] = 3;
+
+        setTimeout(function () { // show alert after response is sent
+            alert('Enter this code in Sync: ' + codeToEnter); // TODO: translate
+        }, 50);
+
+        return {
+            'result': 'RESPOND_TO_CHALLENGE',
+            'nonce': nonce
+        }
+    }
+
+
+    var _respondToResponseToSynchronizationPermittingChallenge = function (requestBody) {
+        var nonce = requestBody.nonce;
+        var enteredChallengeResponse = requestBody.challengeResponse;
+
+        var correctResponse = _nonceToCorrectResponseMap[nonce];
+
+        if(correctResponse == enteredChallengeResponse) {
+            return {
+                'result' : 'ALLOWED',
+                'synchronizationPort' : SyncAppCommunicatorConfig.DeviceDoesNotSupportActiveSynchronization
+            }
+        }
+        else {
+            var countRetriesLeft = _nonceToCountRetriesLeftMap[nonce];
+            countRetriesLeft = countRetriesLeft - 1;
+
+            if(countRetriesLeft > 0) {
+                return {
+                    'result' : 'WRONG_CODE',
+                    'countRetriesLeft' : countRetriesLeft
+                }
+            }
+            else {
+                return {
+                    'result' : 'DENIED'
+                }
+            }
+        }
+    };
+
     function _syncContact(receivedMessage) {
         return AddressBook.handleSynchronizedContact(receivedMessage.contact, receivedMessage.state);
-    }
+    };
+
 
     var _createResponse = function(responseBody, receivedMessage) {
         var response;
