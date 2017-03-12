7138c1164dadf7d2cb005ebe6984624d692103b7
dankito <dankito@user.noreply.github.com>
2017-03-12 11:09:08 +0100

    Implemented createSha512HashAsBase64()

--- thunderbird-plugin/plugin/chrome/content/Util.js
+++ thunderbird-plugin/plugin/chrome/content/Util.js
@@ -22,3 +22,25 @@ function generateUuid() {
         return v.toString(16);
     });
 }
+
+function createSha512HashAsBase64(stringToHash) { // TODO make generic (Hash function and if it should be returned as binary data or Base64)
+    var converter =
+        Components.classes["@mozilla.org/intl/scriptableunicodeconverter"].
+        createInstance(Components.interfaces.nsIScriptableUnicodeConverter);
+
+// we use UTF-8 here, you can choose other encodings.
+    converter.charset = "UTF-8";
+// result is an out parameter,
+// result.value will contain the array length
+    var result = {};
+// data is an array of bytes
+    var data = converter.convertToByteArray(stringToHash, result);
+
+    var cryptoHash = Components.classes["@mozilla.org/security/hash;1"]
+        .createInstance(Components.interfaces.nsICryptoHash);
+    cryptoHash.init(cryptoHash.SHA512);
+    cryptoHash.update(data, data.length);
+
+    var base64Hash = cryptoHash.finish(true); // false = binary, true = Base64
+    return base64Hash.replace(/(?:\r\n|\r|\n)/g, ''); // replace new line chars
+}
